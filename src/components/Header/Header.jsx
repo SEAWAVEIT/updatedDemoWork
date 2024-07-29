0288b23800c9ace70b6cbe910d0d799b445cd3e6
Vedant1402Naik <vedantnaik1402@gmail.com>
2024-07-29 13:54:57 +0530

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -23,18 +23,15 @@ const Header = () => {
     if (open) {
       tl.to(menu, {
         duration: 0.3,
-        autoAlpha: 1, // Animates both opacity and visibility
-        scaleY: 1, // Scales the menu's height
-        transformOrigin: "top",
-        onStart: () => menu.style.display = 'block'
+        height: "auto",
+        width: 198,
+        opacity: 1,
       });
     } else {
       tl.to(menu, {
         duration: 0.3,
-        autoAlpha: 0, // Animates both opacity and visibility
-        scaleY: 0, // Scales the menu's height
-        transformOrigin: "top",
-        onComplete: () => menu.style.display = 'none'
+        height: 0,
+        opacity: 0,
       });
     }
   };
@@ -61,69 +58,83 @@ const Header = () => {
         <div className="navbar-start">
           <div className="dropdown relative">
             <button
-              className="btn btn-ghost lg:hidden flex items-center"
+              className="btn btn-ghost lg:hidden"
               onClick={toggleMenu}
               aria-label="Toggle Menu"
             >
-              {isOpen ? "✖" : "☰"}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
             </button>
-            <ul
-              className={`menu whitespace-nowrap menu-sm dropdown-content rounded-box absolute z-20 mt-3 w-full md:w-80 p-2 shadow bg-black text-white ${isMobile ? "block" : "hidden"}`}
-              ref={menuRef}
-              style={{ transformOrigin: "top", transform: `scaleY(${isOpen ? 1 : 0})`, opacity: isOpen ? 1 : 0 }}
-            >
-              <li>
-                <Link to={"about"}>About</Link>
-              </li>
-              <li>
-                <button
-                  className="w-full text-left"
-                  onClick={toggleServices}
-                  aria-expanded={isServicesOpen}
-                >
-                  Services
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className={`h-5 w-5 inline ml-2 transition-transform ${isServicesOpen ? "rotate-180" : ""}`}
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+            {isOpen && (
+              <ul
+                className={`menu whitespace-nowrap menu-sm dropdown-content rounded-box absolute z-20 mt-3 w-full md:w-80 p-2 shadow bg-black text-white ${isMobile ? "block" : "hidden"}`}
+                ref={menuRef}
+              >
+                <li>
+                  <Link to={"about"}>About</Link>
+                </li>
+                <li>
+                  <button
+                    className="w-full text-left"
+                    onClick={toggleServices}
+                    aria-expanded={isServicesOpen}
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M19 9l-7 7-7-7"
-                    />
-                  </svg>
-                </button>
-                {isServicesOpen && (
-                  <ul className="p-2 bg-black text-white z-1 rounded-lg mt-2">
-                    <li>
-                      <Link to={"customclearance"}>Custom Clearance</Link>
-                    </li>
-                    <li>
-                      <Link to={"freightforwarding"}>Freight Forwarding</Link>
-                    </li>
-                    <li>
-                      <Link to={"landfreight"}>Land Freight</Link>
-                    </li>
-                    <li>
-                      <Link to={"warehousing"}>Warehousing</Link>
-                    </li>
-                    <li>
-                      <Link to={"logisticsdesign"}>Logistics Design</Link>
-                    </li>
-                  </ul>
-                )}
-              </li>
-              <li>
-                <Link to={"contact"}>Contact</Link>
-              </li>
-              <li>
-                <Link to={"getafreequote"}>Get A Free Quote</Link>
-              </li>
-            </ul>
+                    Services
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className={`h-5 w-5 inline ml-2 transition-transform ${isServicesOpen ? "rotate-180" : ""}`}
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M19 9l-7 7-7-7"
+                      />
+                    </svg>
+                  </button>
+                  {isServicesOpen && (
+                    <ul className="p-2 bg-black text-white z-1 rounded-lg mt-2">
+                      <li>
+                        <Link to={"customclearance"}>Custom Clearance</Link>
+                      </li>
+                      <li>
+                        <Link to={"freightforwarding"}>Freight Forwarding</Link>
+                      </li>
+                      <li>
+                        <Link to={"landfreight"}>Land Freight</Link>
+                      </li>
+                      <li>
+                        <Link to={"warehousing"}>Warehousing</Link>
+                      </li>
+                      <li>
+                        <Link to={"logisticsdesign"}>Logistics Design</Link>
+                      </li>
+                    </ul>
+                  )}
+                </li>
+                <li>
+                  <Link to={"contact"}>Contact</Link>
+                </li>
+                <li>
+                  <Link to={"getafreequote"}>Get A Free Quote</Link>
+                </li>
+              </ul>
+            )}
           </div>
           <Link to={"/"} className="btn btn-ghost text-xl">
             Home
